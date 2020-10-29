185b04cc94e36847deff6e9402cf66183f031b25
rakesh-up <72639690+rakesh-up@users.noreply.github.com>
2020-10-30 03:26:24 +0530

    added Redhat version

--- jtest.sh
+++ jtest.sh
@@ -2,3 +2,5 @@
 echo "Hello World on `date`"
 echo "System up time is: `uptime`"
 echo "This script is running under user = `whoami`"
+RVER=$(cat /etc/redhat-release)
+echo "REDHAT Vesrion is : $RVER"
