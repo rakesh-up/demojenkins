73bab501843ba2609634769a8914d05a87a4fe25
Rakesh Upadhyay <rakeshu541@gmail.com>
2020-10-30 15:04:14 +0530

    chnaged check.sh

--- check.sh
+++ check.sh
@@ -3,4 +3,9 @@
 echo "This is to test and script is working"
 echo "Welcome to Jenkins"
 
+RVER=$(cat /etc/redhat-release |cut -d' ' -f7)
+echo "Redhat version is = RHEL $RVER"
+
+echo "System uptime is = `uptime`"
+
 
